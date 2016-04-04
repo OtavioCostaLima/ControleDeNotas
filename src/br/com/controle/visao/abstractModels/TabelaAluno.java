09ee476d091d5c230d5943d03d91566c3c643c54
Otavio Costa <otavio_slz15@hotmail.com>
2016-04-04 16:12:34 -0300

    Alterações Feitas

--- src/br/com/controle/visao/abstractModels/TabelaAluno.java
+++ src/br/com/controle/visao/abstractModels/TabelaAluno.java
@@ -2,6 +2,7 @@ package br.com.controle.visao.abstractModels;
 
 import br.com.controle.util.modelo.Aluno;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
@@ -10,24 +11,26 @@ import javax.swing.table.AbstractTableModel;
  * @author Otavio Costa
  */
 public class TabelaAluno extends AbstractTableModel {
-
+    
     private List<Aluno> alunos = new ArrayList<>();
     private final String colunas[] = {"Matricula", "Nome", "Turma", "Status", "Situação"};
-
+    
     public TabelaAluno() {
-
+        
     }
-
+    
     public void addListaAluno(List<Aluno> alunos) {
+        Collections.sort(alunos);
         this.alunos = alunos;
     }
-
+    
     public void inserirAlunos(List<Aluno> alunos) {
         limparLista();
+        Collections.sort(alunos);
         this.alunos.addAll(alunos);
-
+        
     }
-
+    
     public void addAluno(Aluno aluno) {
         if (alunos == null) {
             alunos = new ArrayList<>();
@@ -35,32 +38,32 @@ public class TabelaAluno extends AbstractTableModel {
         this.alunos.add(aluno);
         fireTableRowsInserted(getRowCount(), getRowCount() - 1);
     }
-
+    
     public Aluno delAluno(int indicde) {
         Aluno aluno = alunos.remove(indicde);
         fireTableRowsDeleted(indicde, indicde);
         return aluno;
     }
-
+    
     public Aluno getAluno(int index) {
         return alunos.get(index);
     }
-
+    
     public void limparLista() {
         alunos.clear();
         fireTableDataChanged();
     }
-
+    
     @Override
     public int getRowCount() {
         return alunos.size();
     }
-
+    
     @Override
     public int getColumnCount() {
         return colunas.length;
     }
-
+    
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         Aluno aluno = alunos.get(rowIndex);
@@ -79,15 +82,15 @@ public class TabelaAluno extends AbstractTableModel {
                 return null;
         }
     }
-
+    
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
         return false;
     }
-
+    
     @Override
     public String getColumnName(int column) {
         return colunas[column];
     }
-
+    
 }
