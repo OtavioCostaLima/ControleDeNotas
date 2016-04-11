3bbda072e4b8867d88d052e68deb71201a4db48b
Suporte Técnico <Suporte Tcnico@PC-01>
2016-04-11 12:14:40 -0300

    Merge origin/master
    Conflicts:
    	src/br/com/controle/visao/GerenteDeArquivos.java

--- src/br/com/controle/util/ReportUtil.java
+++ src/br/com/controle/util/ReportUtil.java
@@ -5,10 +5,26 @@
  */
 package br.com.controle.util;
 
+import java.util.List;
+import java.util.Map;
+import javax.swing.JDialog;
+
 /**
  *
  * @author Suporte Técnico
  */
 public class ReportUtil {
-    
+    //mandando para o Jasper
+
+    public void imprimeRelatorioJasper(String arquivo, List list, Map parametros) 
+        JRException, SQLException{
+               java.io.InputStream file = getClass().getClassLoader().getResourceAsStream(arquivo);
+
+        JRDataSource dados = new JRBeanCollectionDataSource(list);
+        JasperPrint printer = JasperFillManager.fillReport(file, parametros, dados);
+        JRViewer view = new JRViewer(printer);
+        JDialog dialog = new JDialog();
+        dialog.getContentPane().add(view);
+        dialog.setVisible(true);
+    }
 }
